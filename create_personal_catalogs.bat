9b522fbd7488007ef21c6fb49d631577f716b868
Lena <lenafil2002@gmail.com>
2026-05-17 15:10:26 +0300

    Создала скрипт с создание папок

---
+++ create_personal_catalogs.bat
@@ -0,0 +1,14 @@
+echo off
+rem создаю папку с фамилией
+md Filimonova
+cd Filimonova
+
+rem создаю папку с именем
+md Elena
+cd Elena
+
+rem создаю папку с отчеством
+md Aleksandrovna
+cd Aleksandrovna
+
+pause
