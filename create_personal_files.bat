0af6de672118e1cd0813021b4af34a1201632ebe
Lena <lenafil2002@gmail.com>
2026-05-17 15:17:14 +0300

    Создала скрипт с созданием файлов

---
+++ create_personal_files.bat
@@ -0,0 +1,20 @@
+echo off
+rem заходим в папку с фамилией
+cd Filimonova
+
+rem создаём файл с датой рождения
+echo > 25062005.txt
+
+rem заходим в папку с именем
+cd Elena
+
+rem создаём файл
+echo > hello.txt
+
+rem заходим в папку с отчеством
+cd Aleksandrovna
+
+rem создаём файл с номером компьютера
+echo > pc05.txt
+
+pause
